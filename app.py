589c4accdec7efa9be63f081df572d88c45d65fd
Ibrahim Mert Akbas <akbas.ibrahim641@gmail.com>
2025-12-15 19:02:14 +0300

    Diamond Price Predictor

---
+++ app.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Request
+import pickle
+import pandas as pd
+from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+with open("diamond_model_complete.pkl","rb") as file:
+    saved_data = pickle.load(file)
+    model = saved_data["model"]
+    encoders = saved_data["encoders"]
+    scaler = saved_data["scaler"]
+
+class DiamondFeatures(BaseModel):
+    carat: float
+    cut: str
+    color: str
+    clarity: str
+    depth: float
+    table: float
+    x: float
+    y: float
+    z: float
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/predict")
+async def predict(features: DiamondFeatures):
+    input_data = pd.DataFrame([features.model_dump()])
+
+    for col in ["cut","color","clarity"]:
+        input_data[col] = encoders[col].transform(input_data[col])
+
+    input_scaled = scaler.transform(input_data)
+    prediction = model.predict(input_scaled)
+
+    return {"predicted_price": prediction[0]}
